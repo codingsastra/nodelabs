f7138eb6b388d16e69249855112f2e02315d2733
Varma Bhupatiraju <varmab@gmail.com>
2019-11-20 12:06:14 +0530

    mongoose complete

--- routes/students.js
+++ routes/students.js
@@ -16,6 +16,41 @@ router.route("/")
             res.send(student);
         })
     })
+
+router.route("/:id")
+    .get((req,res)=>{
+
+        var id=req.params.id;
+        db.Student.findById(id,function(err,s){
+            if(err) res.send("Error" + JSON.stringify(err));
+            res.send(s);
+        })
+
+    })
+    .delete((req,res)=>{
+        var id=req.params.id;
+        db.Student.findByIdAndDelete(id,function(err,student){
+            if(err) res.send("Error" + JSON.stringify(err));
+            res.send(student);
+        })
+    })
+    .put((req,res)=>{
+        var id=req.params.id;
+        db.Student.findByIdAndUpdate(id,req.body,{new:true},function(err,student){
+            if(err) res.send("Error" + JSON.stringify(err));
+            res.send(student);
+        })
+    })
+
+router.route("/search/:keyword")
+    .get((req,res)=>{
+        var keyword=req.params.keyword;
+
+        db.Student.find({firstName: { $regex: '.*' + keyword + '.*' }},function(err,students){
+            if(err) res.send("Error");
+            res.send(students);
+        })
+    })
     
 
 module.exports=router;
